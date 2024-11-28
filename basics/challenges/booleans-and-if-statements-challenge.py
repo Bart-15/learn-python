174d1a6e881ce1f3356b38d4e1fd3f8bd5352c46
Bart-15 <brat.tabusao@gmail.com>
2024-11-28 14:46:32 +0800

    modify return for booleans-and-if-statements-challenge

--- basics/challenges/booleans-and-if-statements-challenge.py
+++ basics/challenges/booleans-and-if-statements-challenge.py
@@ -1,7 +1,7 @@
 # You tu aride at theme park and can only let people 48 and taller ride the ride. Print yes or no depending one someone's height
 
-height = 48
+height >= 48
 if height == 48:
-  print('Yes')
+  print('Yes, you can ride')
 else:
   print('No')
