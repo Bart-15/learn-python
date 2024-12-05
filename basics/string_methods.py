2c1036675946cb812994c60d7d4f8c55a7741dce
Bart-15 <brat.tabusao@gmail.com>
2024-12-05 17:38:50 +0800

    feat: string_methods

---
+++ basics/string_methods.py
@@ -0,0 +1,31 @@
+text = "Hello world!"
+
+# upper case
+result = text.upper()
+
+# lower
+result = text.lower()
+
+# split
+result = text.split()
+
+print(result)
+
+# join
+a = 'Learning'
+b = 'Python'
+c = 'is'
+d = 'Amazing'
+
+joined_string = " ".join([a,b,c,d])
+
+print(joined_string)
+
+find_s = joined_string.find("q")
+
+print(find_s)
+
+# replace
+replace_result = joined_string.replace('e', 'y')
+
+print(replace_result)
