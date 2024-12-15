9f55f28e6b742d14ac9b7a1845814bbc5e3726c6
Bart-15 <brat.tabusao@gmail.com>
2024-12-15 18:35:34 +0800

    add more examples on polymorphism

--- fundamentals/oop/polymorphism.py
+++ fundamentals/oop/polymorphism.py
@@ -21,3 +21,19 @@ def animal_talks(animal):
   animal.talk()
 
 animal_talks(sheep1)
+
+a_word = "polymorphism"
+a_list = ["Classes", "OOP", "Polymorphism"]
+a_tuple = (1, 2, 3, 80)
+
+def get_length(list_data):
+  total_length = len(list_data)
+
+  return print(total_length)
+    
+iterate_list = [a_word, a_list, a_tuple]
+    
+
+for data in iterate_list:
+  get_length(data)
+    
