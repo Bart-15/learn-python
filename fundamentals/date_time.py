a232f9ddcb2665a5256ad2dafa7bc2ed1ec8d281
Bart-15 <brat.tabusao@gmail.com>
2024-12-17 19:53:22 +0800

    add: dat_time and math

---
+++ fundamentals/date_time.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+my_day = datetime(2025, 10, 17, 22, 10, 15, 2500)
+
+print(my_day)
---
+++ fundamentals/math_module.py
@@ -0,0 +1,8 @@
+import math
+
+result = math.floor(89.90)
+
+print(result)
+
+result2 = math.ceil(78.12)
+print(result2)
